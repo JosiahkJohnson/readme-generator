a4bfccb9c58997dc3e28a4b56eccc4c96fa3c75a
Josiah <josiahkj123@gmail.com>
2020-02-24 17:29:11 -0600

    Working prototype created, hardcoded readme

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
+const util = require("util");
 
-inquirer
-  .prompt({
-    message: "Enter your GitHub username:",
-    name: "username"
-  })
-  .then(function({ username }) {
-    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+//set a variable to write a new readme.md file
+const writeFileAsync = util.promisify(fs.writeFile);
 
-    axios.get(queryUrl).then(function(res) {
-      const repoNames = res.data.map(function(repo) {
-        return repo.name;
-      });
+//main function that will create the skeleton of the readme
+function createReadme(info){
+    //All this will be written into the readme
+    return `
+    # ${info.title}
+    [![made-for-VSCode](https://img.shields.io/badge/Made%20for-VSCode-1f425f.svg)](https://code.visualstudio.com/)
+    
+    ## Table of Contents
+    1.[ ### Description](#Description)
 
-      const repoNamesStr = repoNames.join("\n");
+    ## Description:<a name = "Description"></a>
+    ${info.description}
+    `
+}
 
-      fs.writeFile("repos.txt", repoNamesStr, function(err) {
-        if (err) {
-          throw err;
-        }
+const response ={
+    title: "Awesome Project",
+    description: "This is a test description for my awesome project that will automatically create these readmes so I don't have to."
+}
 
-        console.log(`Saved ${repoNames.length} repos`);
-      });
-    });
-  });
+//aync function so I can use await
+async function writeReadme(){
+    const markdown = createReadme(response);
+    await writeFileAsync("README.MD", markdown);
+}
+
+writeReadme();
